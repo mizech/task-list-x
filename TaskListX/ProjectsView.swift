8ca8ca960f793607a74e68e5e7e71b5f02d38251
mizech <michael.zech@hotmail.de>
2025-10-03 16:23:36 +0200

    Added: Views

---
+++ TaskListX/ProjectsView.swift
@@ -0,0 +1,18 @@
+//
+//  ProjectsView.swift
+//  TaskListX
+//
+//  Created by Michael on 03.10.25.
+//
+
+import SwiftUI
+
+struct ProjectsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProjectsView()
+}
---
+++ TaskListX/TasksView.swift
@@ -0,0 +1,18 @@
+//
+//  TasksView.swift
+//  TaskListX
+//
+//  Created by Michael on 03.10.25.
+//
+
+import SwiftUI
+
+struct TasksView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TasksView()
+}
