6dd9b5e8232e13194a9164a116572d7ecf21cb06
mizech <michael.zech@hotmail.de>
2025-10-03 16:07:25 +0200

    Initial Commit

---
+++ TaskListX/TaskListXApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskListXApp.swift
+//  TaskListX
+//
+//  Created by Michael on 03.10.25.
+//
+
+import SwiftUI
+
+@main
+struct TaskListXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
