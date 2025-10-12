028c8574e35d181ea3964562061ea3b9a2b71d8d
mizech <michael.zech@hotmail.de>
2025-10-12 14:42:53 +0200

    Impl.: ProjectFormView

---
+++ TaskListX/Views/ProjectDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  ProjectDetailsView.swift
+//  TaskListX
+//
+//  Created by Michael on 11.10.25.
+//
+
+import SwiftUI
+
+struct ProjectDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProjectDetailsView()
+}
---
+++ TaskListX/Views/ProjectFormView.swift
@@ -0,0 +1,18 @@
+//
+//  ProjectFormView.swift
+//  TaskListX
+//
+//  Created by Michael on 12.10.25.
+//
+
+import SwiftUI
+
+struct ProjectFormView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProjectFormView()
+}
