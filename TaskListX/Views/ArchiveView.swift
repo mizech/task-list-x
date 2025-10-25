43083ef30c8a57a2b5af88db25b5823c6f87d4e7
mizech <michael.zech@hotmail.de>
2025-10-25 15:01:02 +0200

    Impl.: TabView selected

---
+++ TaskListX/Views/ArchiveView.swift
@@ -0,0 +1,18 @@
+//
+//  ArchiveView.swift
+//  TaskListX
+//
+//  Created by Michael on 20.10.25.
+//
+
+import SwiftUI
+
+struct ArchiveView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ArchiveView()
+}
---
+++ TaskListX/Views/TaskFormView.swift
@@ -0,0 +1,18 @@
+//
+//  TaskFormView.swift
+//  TaskListX
+//
+//  Created by Michael on 20.10.25.
+//
+
+import SwiftUI
+
+struct TaskFormView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskFormView()
+}
